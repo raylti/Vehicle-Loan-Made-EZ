e2e689da5da1fc43b97220b825800a6aeceae11b
abhinavm20 <abhinavm20@gmail.com>
2020-09-18 22:49:07 +0530

    3 component added

--- Angular-part/src/app/app.module.ts
+++ Angular-part/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginDashboardComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,

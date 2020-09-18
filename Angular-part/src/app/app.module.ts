caccd9136d9893eb340221fd20b08a139a49b7ef
Yugandhara123 <rajhans.yugandhara@lntinfotech.com>
2020-09-18 23:16:59 +0530

    my message

--- Angular-part/src/app/app.module.ts
+++ Angular-part/src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 import { AppForm1Component } from './app-form1/app-form1.component';
+
 import { LoginComponent } from './login/login.component';
 import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+
     AppForm1Component,
+
     LoginComponent,
     LoginDashboardComponent,
     AdminDashboardComponent
+
   ],
   imports: [
     BrowserModule,

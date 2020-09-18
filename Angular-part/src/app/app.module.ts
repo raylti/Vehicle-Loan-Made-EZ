fb2096a654d44304da6dfe363b1de5adeb4d2c1d
abhinavm20 <abhinavm20@gmail.com>
2020-09-18 23:15:18 +0530

    appmodule commit

--- Angular-part/src/app/app.module.ts
+++ Angular-part/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-
-import { HomeComponent } from './home/home.component';
-
-
-import { AppForm1Component } from './app-form1/app-form1.component';
-
-
-=======
 import { HomeComponent } from './home/home.component';
 
 import { AppForm1Component } from './app-form1/app-form1.component';
 
->>>>>>> 81c68097c520c3d6c9b5f713ddac8d5b677de79c
 import { LoginComponent } from './login/login.component';
 import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';

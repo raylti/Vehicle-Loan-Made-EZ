1c4c6409c9ae4a6efd1c63afc02288db64def8a0
Yugandhara123 <rajhans.yugandhara@lntinfotech.com>
2020-09-18 23:05:05 +0530

    my commit

--- Angular-part/src/app/app.module.ts
+++ Angular-part/src/app/app.module.ts
@@ -3,24 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AppForm1Component } from './app-form1/app-form1.component';
-=======
 import { LoginComponent } from './login/login.component';
 import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
->>>>>>> e2e689da5da1fc43b97220b825800a6aeceae11b
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    AppForm1Component
-=======
+    AppForm1Component,
     LoginComponent,
     LoginDashboardComponent,
     AdminDashboardComponent
->>>>>>> e2e689da5da1fc43b97220b825800a6aeceae11b
   ],
   imports: [
     BrowserModule,

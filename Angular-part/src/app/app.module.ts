e5ab9718aa3495ac156a6377952358dfea1d0e02
rahulkr-lti <rahulkumarsingh1001@gmail.com>
2020-09-20 20:19:26 +0530

    Home,emiCalc,applyLoan,loanOffer added

--- Angular-part/src/app/app.module.ts
+++ Angular-part/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+import { Ng5SliderModule } from 'ng5-slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +44,9 @@ import { LoanOfferComponent } from './loan-offer/loan-offer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng5SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
